bb34cf641d2948b93569a2d1dfde127b87fb34ea
tranminhkhoadev <tranminhkhoa.dev@gmail.com>
2026-02-26 20:56:32 +0700

    Update Baitap_day6.js

--- JavaScript/Day6/Baitap_day6.js
+++ JavaScript/Day6/Baitap_day6.js
@@ -12,3 +12,13 @@ function introduce(name, age, address) {
 }
 
 introduce("Khoa", 15, "Canada");
+
+
+function calculatePytago(canhGocVuong1, canhGocVuong2) {
+    const binhPhuong1 = Math.pow(canhGocVuong1, 2);
+    const binhPhuong2 = Math.pow(canhGocVuong2, 2);
+    const canhHuyen = Math.sqrt(binhPhuong1 + binhPhuong2);
+    return canhHuyen;
+}
+
+console.log(`Cạnh huyền có kết quả là: ${calculatePytago(3, 4)}`);
