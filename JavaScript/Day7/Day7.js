5ac7ebf07274152cda98178e4e1db888d83ec934
tranminhkhoadev <tranminhkhoa.dev@gmail.com>
2026-02-28 08:17:33 -0500

    Update Day7.js

--- JavaScript/Day7/Day7.js
+++ JavaScript/Day7/Day7.js
@@ -1,5 +1,5 @@
 const carts = ["Áo", "Đồ ăn", "Xe", "Điện thoại", "Trang Sức", "Quần", "Sách", "Kính"];
 
 console.log(carts.length);
-console.log(carts[1]);
+console.log(carts[0]);
 console.log(carts[carts.length - 1]);
