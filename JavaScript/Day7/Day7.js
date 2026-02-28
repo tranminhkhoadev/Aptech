7273ed2d28bc8a10452046f1a2295273710df689
tranminhkhoadev <tranminhkhoa.dev@gmail.com>
2026-02-28 09:20:12 -0500

    Update Day7.js

--- JavaScript/Day7/Day7.js
+++ JavaScript/Day7/Day7.js
@@ -1,5 +1,13 @@
-const carts = ["Áo", "Đồ ăn", "Xe", "Điện thoại", "Trang Sức", "Quần", "Sách", "Kính"];
+//const carts = ["Áo", "Đồ ăn", "Xe", "Điện thoại", "Trang Sức", "Quần", "Sách", "Kính"];
 
-console.log(carts.length);
-console.log(carts[0]);
-console.log(carts[carts.length - 1]);
+//console.log(carts.length);
+//console.log(carts[0]);
+//console.log(carts[carts.length - 1]);
+
+
+
+const friends = ["Hùng", "Thanh", "Khanh"]
+
+friends.push("Lộc")
+console.log(friends.length);
+console.log(friends[1]);
