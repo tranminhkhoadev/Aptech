eac8ae8ab1ccedf5f58a6a09377f14d69eaa4c82
tranminhkhoadev <tranminhkhoa.dev@gmail.com>
2026-03-03 20:09:21 +0700

    Create Day8.js

---
+++ JavaScript/Day8/Day8.js
@@ -0,0 +1,14 @@
+
+const tinhThanh = [
+  "Hà Nội",
+  "Hồ Chí Minh",
+  "Đà Nẵng",
+  "Cần Thơ",
+  "Hải Phòng",
+  "Đồng Nai"
+];
+
+// Sử dụng forEach để in từng tỉnh/thành
+tinhThanh.forEach(function(tinh) {
+  console.log(tinh);
+});
